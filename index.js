55333e064e96b54f43fb6836722ef53070d2ed6f
‘Gonzo <gonzonieto@gmail.com>
2022-03-19 18:29:51 +0000

    working solution completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // will require and run our main fetch function
+
+const nextISSTimesForMyLocation = () => {
+  const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+
+  fetchMyIP((fetchIPError, ip) => {
+    if (fetchIPError) return console.log("Unable to fetch IP. Error:", fetchIPError);
+
+    fetchCoordsByIP(ip, (fetchCoordsError, geoData) => {
+      if (fetchCoordsError) return console.log("Unable to fetch coordinates. Error: " + fetchCoordsError);
+
+      console.log("IP:", ip);
+      console.log();
+      console.log("Latitude:", geoData.latitude);
+      console.log("Longitude:", geoData.longitude);
+      console.log();
+
+      fetchISSFlyOverTimes(geoData, (flyOverError, flyOverTimes) => {
+        if (flyOverError) return console.log(flyOverError);
+
+        flyOverTimes.forEach((pass) => {
+          const dateTime = new Date(0);
+          dateTime.setUTCSeconds(pass.risetime);
+          
+          const duration = pass.duration;
+          console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+          // console.log(`Next pass at ${dateTime.toLocaleString('en-GB', { timezone: "America/Toronto"})} for ${duration} seconds!`);
+        });
+
+        // console.log("\nIslamic calendar:");
+        // flyOverTimes.forEach((pass) => {
+        //   const dateTime = new Date(0);
+        //   dateTime.setUTCSeconds(pass.risetime);
+          
+        //   const duration = pass.duration;
+        //   console.log(`Next pass at ${dateTime.toLocaleString('en-GB-u-ca-islamic', { timezone: "America/Toronto"})} for ${duration} seconds!`);
+        // });
+      });
+    });
+  });
+};
+
+nextISSTimesForMyLocation();
--- iss.js
+++ iss.js
@@ -1,1 +1,54 @@
 // will contain most of the logic for fetching the data from each API endpoint
+
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  request('https://api.ipify.org/?format=json', (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}.`;
+      return callback(Error(msg), null);
+    }
+
+    if (body) {
+      callback(null, JSON.parse(body).ip);
+    }
+  });
+};
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://api.freegeoip.app/json/${ip}?apikey=a132a880-a752-11ec-adf2-73450cb58a54`, (error, response, geoData) => {
+    if (error) return callback(error, null);
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates. Response: ${geoData}.`;
+      return callback(Error(msg), null);
+    }
+
+    callback(null, JSON.parse(geoData));
+  });
+};
+
+const fetchISSFlyOverTimes = (geoData, callback) => {
+  const lat = geoData.latitude;
+  const lon = geoData.longitude;
+  request(`https://iss-pass.herokuapp.com/json/?lat=${lat}&lon=${lon}`, (error, response, flyOver) => {
+  
+    if (error) {
+      console.log("In here!");
+      return callback(error, null);
+    };
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching flyover times. Response: ${flyOver}.`;
+      return callback(Error(msg), null);
+    }
+    
+    callback(null, JSON.parse(flyOver).response);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
